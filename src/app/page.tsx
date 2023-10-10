aac79a99bb6af61d1c0303c7bbe95fafb230c488
clive t <clivethompson@gmail.com>
2023-10-10 14:47:11 +0100

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Navbar from '../components/Navbar'
+import CallToAction from '../components/CallToAction'
+import Profile from '../components/Profile'
+export default function Home() {
+  return (
+	<>
+	<Navbar />
+	<div className="py-10">
+		<CallToAction />
+	</div>
+	<Profile />
+	</>
+  )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+export default function Profile(){
+	return(
+
+	<Image
+	src={imageLoader}
+	width={500}
+	height={500}
+	alt=""
+	/>
+
+)}
+
+
